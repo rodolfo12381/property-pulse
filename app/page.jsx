4367a68dd190512ae9c777b9b1d2976243797ad5
Rodolfo Rocha Rodrigues <rodolfo.rocha@HINOVA.net>
2024-03-11 19:56:38 -0300

    adicionando modificações na page principal

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Link from "next/link";
 
-export default function HomePage() {
-  return <div>HomePage</div>;
-}
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Welcome</h1>
+      <Link href="/properties">Show Properties</Link>
+    </div>
+  );
+};
+
+export default HomePage;
