0849726d240d62a722a730886140a58619e071ec
Rodolfo Rocha <rodolforrodrigues14@gmail.com>
2024-04-07 14:06:43 -0300

    integrando backend e adicionando pagina de property details

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+//Fetch all properties
 async function fetchProperties() {
     try {
 
@@ -17,4 +18,22 @@ async function fetchProperties() {
     }
 }
 
-export { fetchProperties };
+//Fetch single property
+async function fetchProperty(id) {
+    try {
+
+        if (!apiDomain) {
+            return null;
+        }
+        const res = await fetch(`${apiDomain}/properties/${id}`);
+        if (!res.ok) {
+            throw new Error('Failed to fetch data');
+        }
+        return res.json();
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+export { fetchProperties, fetchProperty };
